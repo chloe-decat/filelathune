e10f68e1e551e52329b1f71ec4a85d8c66144ac2
chloe bataille <chloe.bataille@decathlon.com>
2018-01-03 15:03:41 +0100

    layout

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log("toto");
+const express = require("express");
+const nunjucks = require("nunjucks");
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app
+});
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "njk");
+
+app.get("/", function(request, result) {
+  result.render("login");
+});
+app.listen(3000)
