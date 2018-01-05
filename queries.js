998cab18a4b760767124a26a91852b81b8fa5350
chloe bataille <chloe.bataille@decathlon.com>
2018-01-05 16:01:37 +0100

    add connexion fbk BDD

--- queries.js
+++ queries.js
@@ -19,21 +19,36 @@ function insertUser(name, email, password){
 }
 
 
-function findOrCreateUser(name,email){
-  console.log("je suis dans find0rcreate")
- const client = new PG.Client();
- client.connect();
- return client.query(
- "INSERT INTO users (id, name, email) VALUES (uuid_generate_v4(), $1::text, $2::text)",
- [name, email])
-   .then(result => {
-     return  console.log("insert OK");;
-   })
-   .catch(error => console.log(error))
- ;
- client.end();
-}
 
+function findOrCreateUser(profile, callback){
+  const facebook_id = profile.id;
+  const facebook_name = profile.displayName;
+  const client = new PG.Client();
+  client.connect();
+
+  return client.query(
+    "SELECT EXISTS(SELECT facebook_id FROM users WHERE facebook_id=$1)",
+    [facebook_id])
+  .then(result => {
+    if (result.rows[0].exists === true) {
+      console.log(profile)
+      callback(null, profile)
+    } else {
+      return client.query(
+        "INSERT INTO users (id, facebook_id, name) VALUES (uuid_generate_v4(), $1::text, $2::text)",
+        [facebook_id, facebook_name])
+      .then(_ => {
+        client.end();
+        callback(null, profile);
+      })
+      .catch(error => {
+        callback(error);
+      })
+    }
+  })
+  .catch(error => console.log(error))
+}
+// Rajouter le find sinon a chaque connexion, facebook va poster un utilisateur dans ma bdd voire erreur car l'id est supposé unique.
 
 module.exports = {
   insertUser: insertUser,
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const nunjucks = require("nunjucks");
 const sha256 = require("sha256");
-
 const queries = require("./queries.js");
 const users = require("./user.js");
 const FacebookStrategy = require("passport-facebook").Strategy;
@@ -38,10 +37,8 @@ passport.serializeUser(function(user, callback) {
   return callback(null, user.id);
 });
 
-passport.deserializeUser(function(id, callback) {
-  return users.findUserById(id).then(user => {
-    callback(null, user)
-  });
+passport.deserializeUser(function(user, callback) {
+  return callback(null, user)
 });
 
 passport.use(
@@ -64,21 +61,7 @@ passport.use(
       callbackURL: process.env.REDIRECT_URI
     },
     function(accessToken, refreshToken, profile, callback) {
-      console.log("je suis dans facebook strategy")
-      FB.api(
-          "me",
-          { fields: "id,name,email", access_token: accessToken },
-          function(user) {
-            console.log("je suis dans facebook strategy/user")
-            findOrCreateUser(user)
-              .then(user => {
-                callback(null, user);
-              })
-              .catch(error => {
-                callback(error);
-              })
-          }
-        );
+      return queries.findOrCreateUser(profile, callback)
     }
   )
 );
@@ -101,8 +84,8 @@ app.get(
   "/auth/facebook/return",
   passport.authenticate("facebook", { failureRedirect: "/login" }),
   function(request, result) {
-      console.log("je suis dans passport.authenticate /login/facebook/return")
-    result.redirect("/");
+    console.log("je suis dans passport.authenticate /login/facebook/return")
+    result.redirect("/profile");
   }
 );
 
@@ -110,67 +93,12 @@ app.get(
   "/profile",
   require("connect-ensure-login").ensureLoggedIn(),
   function(request, result) {
-    console.log("je suis dans passport.authenticate /profile")
     result.render("profile", {
-      id: request.user.id,
-      name: request.user.displayName,
-      emails: request.user.emails
+      user: request.user
     });
-});
+  }
+);
 
-// app.get("/", function(request, result) {
-//   result.render("login");
-// });
-//
-// app.get("/register", function(request, result) {
-//   result.render("register");
-// });
-//
-// app.post(
-//   "/register",
-// //ajouter du code ici pour connecter le user
-// );
-//
-// app.post(
-//   "/",
-//   passport.authenticate("local", { failureRedirect: "/" }),
-//   function(request, result) {
-//     console.log("redirect to /profile");
-//     result.redirect("/profile");
-//   }
-// );
-//
-// app.get(
-//   "/profile",
-//   require("connect-ensure-login").ensureLoggedIn("/"),
-//   function(request, result) {
-//     console.log("toto", request.user)
-//     result.render("profile", {
-//       id: request.user.id,
-//       name: request.user.displayName,
-//       email: request.user.email
-//     });
-//   }
-// );
-//
-// app.get("/logout", function(request, result) {
-//   request.logout();
-//   result.redirect("/");
-// });
-//
-// app.get("/account", function(request, result) {
-//   result.render("account");
-// });
-//
-// app.post(
-//   "/account",
-//   function(request, result) {
-//     queries.insertUser(request.body.name, request.body.username, sha256(request.body.password));
-//     result.redirect("/register");
-//   }
-// );
-//
-//
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log(`Server listening on port ${port}`);
