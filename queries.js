0344fd4afeb3181c7f02a6a0071c04c88c546fa4
Fabien Lebas <fabien.lebas@decathlon.com>
2018-01-06 11:57:58 +0100

    Supprime la fonction insertUser avec callback

--- queries.js
+++ queries.js
@@ -58,23 +58,6 @@ function insertIntoExpenses(name, description, amount, uuid, idActivity, listUse
     })
 }
 
-function insertUser(name, email, password){
-  const client = new PG.Client();
-  client.connect();
-  client.query(
-  "INSERT INTO users (id, name, email, password) VALUES (uuid_generate_v4(), $1::text, $2::text, $3::text)",
-  [name, email, password],
-  function(error, result) {
-    if (error) {
-      console.warn(error);
-    } else {
-      console.log("insert OK");
-      return(result);
-    }
-    client.end();
-  }
-  );
-}
 function insertActivity(uuid, startdate, description, titre, user_id) {
   const client = new PG.Client();
   client.connect();
